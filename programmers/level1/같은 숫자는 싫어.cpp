e74508caf2fcf6cb4da68e812b2331c294bf6503
smy999 <33407191+smy999@users.noreply.github.com>
2020-09-15 17:08:00 +0900

    Create 같은 숫자는 싫어.cpp

---
+++ programmers/level1/같은 숫자는 싫어.cpp
@@ -0,0 +1,21 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12906
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]!=arr[i+1]){
+            answer.push_back(arr[i]);
+        }
+    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    //cout << "Hello Cpp" << endl;
+
+    return answer;
+}
