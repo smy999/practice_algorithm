9e7656d68bd0bc6718194878e31239850a807c99
smy999 <33407191+smy999@users.noreply.github.com>
2020-09-15 17:22:31 +0900

    Create 문자열 내 마음대로 정렬하기.js

---
+++ programmers/level1/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,14 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12915?language=javascript
+
+function solution(strings, n) {
+    
+    return strings.sort((x, y) => {
+        if(x[n] > y[n]) return 1;
+        if(x[n] < y[n]) return -1;
+
+        if(x > y) return 1;
+        if(x < y) return -1;
+
+        return 0;
+    });
+}
