121e77610728c7a4c5a1ab7abdbf7d9d780ab24a
smy999 <33407191+smy999@users.noreply.github.com>
2020-09-15 17:19:47 +0900

    Create 두 정수 사이의 합.js

---
+++ programmers/level1/두 정수 사이의 합.js
@@ -0,0 +1,7 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12912?language=javascript
+
+function solution(a, b) {
+    var answer = 0;
+    answer=((a+b)*(Math.abs(b-a)+1))/2
+    return answer;
+}
