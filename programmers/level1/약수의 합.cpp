8a17b1e9509fea1cb5c823e50feec7abbaf145dd
smy999 <33407191+smy999@users.noreply.github.com>
2020-09-17 12:56:48 +0900

    Create 약수의 합.cpp

---
+++ programmers/level1/약수의 합.cpp
@@ -0,0 +1,19 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12928
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int i=1; i <= n/2; i++){
+        if(n % i == 0)
+            answer += i;
+    }
+    
+    answer += n;
+    
+    return answer;
+}
