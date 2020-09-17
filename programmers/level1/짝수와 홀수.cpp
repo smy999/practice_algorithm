e8ef434bd4dbf978a90b524cc52c2fdab69b19e5
smy999 <33407191+smy999@users.noreply.github.com>
2020-09-17 13:04:05 +0900

    Create 짝수와 홀수.cpp

---
+++ programmers/level1/짝수와 홀수.cpp
@@ -0,0 +1,17 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12937
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    
+    if(num % 2 == 0)
+        answer = "Even";
+    else
+        answer = "Odd";
+    
+    return answer;
+}
