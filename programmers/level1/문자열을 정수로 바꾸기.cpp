01b410094adeda4c65b0c399f6b3b3184c6f1778
smy999 <33407191+smy999@users.noreply.github.com>
2020-09-17 12:55:20 +0900

    Create 문자열을 정수로 바꾸기.cpp

---
+++ programmers/level1/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,10 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12925
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    return stoi(s);
+}
