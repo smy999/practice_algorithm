b00aa9e96b27ca709e323be64edb42c8f9f55d44
smy999 <33407191+smy999@users.noreply.github.com>
2020-09-18 14:29:09 +0900

    Create 행렬의 덧셈.cpp

---
+++ programmers/level1/행렬의 덧셈.cpp
@@ -0,0 +1,15 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12950
+
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer = arr1;
+    
+    for (int i = 0; i < answer.size(); i++)
+        for (int j = 0; j < answer[i].size(); j++)
+            answer[i][j] = arr1[i][j] + arr2[i][j];
+
+    return answer;
+}
