1ce908c27d5d7f6e6f19b84a605795c01a930d10
smy999 <33407191+smy999@users.noreply.github.com>
2020-10-06 15:20:14 +0900

    Create 10869.cpp

---
+++ baekjoon/입출력과 사칙연산/10869.cpp
@@ -0,0 +1,15 @@
+#include <iostream> //C++ 표준 입출력 헤더파일
+
+using namespace std;
+
+int main() {
+	/* #10869*/
+	//unsigned int A, B;
+	int A, B;
+	cin >> A >> B;
+	cout << A + B << "\n";
+	cout << A - B << "\n";
+	cout << A * B << "\n";
+	cout << A / B << "\n";
+	cout << A % B;
+}
