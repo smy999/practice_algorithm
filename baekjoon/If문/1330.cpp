af5cde39b3a5e2cf99da0274e54aa2b45a842d74
smy999 <33407191+smy999@users.noreply.github.com>
2020-10-06 15:28:35 +0900

    Create 1330 두 수 비교하기

---
+++ baekjoon/If문/1330.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	/* #1330*/
+	int A, B;
+	cin >> A >> B;
+	if (A > B) cout << ">";
+	if (A < B) cout << "<";
+	if (A == B)cout << "==";
+}
