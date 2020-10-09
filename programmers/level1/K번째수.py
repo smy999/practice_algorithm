cfb9e62fbfd0b5dce2707e25e5dff1e38f21ea1e
smy999 <33407191+smy999@users.noreply.github.com>
2020-10-09 18:56:16 +0900

    Update K번째수.py

--- programmers/level1/K번째수.py
+++ programmers/level1/K번째수.py
@@ -1,4 +1,6 @@
-def solution(array, commands):
+# 풀이1
+
+def solution1(array, commands):
     answer = []
     
     for i in range(len(commands)):
@@ -7,3 +9,10 @@ def solution(array, commands):
         cut.sort()
         answer += [cut[commands[i][2]-1]]
     return answer
+
+########################################################
+
+# 풀이2
+
+def solution2(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
