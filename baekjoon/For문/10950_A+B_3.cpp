248cb6a4e4483e8a8b079ee16c6271fc501b6d6e
smy999 <33407191+smy999@users.noreply.github.com>
2020-12-29 10:13:20 +0900

    Create 10950_A+B_3.cpp

---
+++ baekjoon/For문/10950_A+B_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	/* #10950*/
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int A, B;
+		cin >> A >> B;
+        cout << A+B << "\n";
+    }
+}
