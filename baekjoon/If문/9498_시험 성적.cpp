bb040ecb6c699bdeffb5160115c13bb603324e48
smy999 <33407191+smy999@users.noreply.github.com>
2020-12-29 10:08:49 +0900

    Create 9498_시험 성적.cpp

---
+++ baekjoon/If문/9498_시험 성적.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	/* #9498*/
+	int A;
+	cin >> A;
+	if (90 <= A) { 
+		cout << "A"; 
+	} else if (80 <= A) { 
+		cout << "B"; 
+	} else if (70 <= A) { 
+		cout << "C"; 
+	} else if (60 <= A) { 
+		cout << "D"; 
+	} else { 
+		cout << "F"; 
+	}
+}
