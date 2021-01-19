1b5709a4425d0d4310462a2c1bdf1224214d7de4
smy999 <33407191+smy999@users.noreply.github.com>
2021-01-19 15:36:57 +0900

    Create 10171_고양이.java

---
+++ baekjoon/입출력과 사칙연산/10171_고양이.java
@@ -0,0 +1,10 @@
+// 10171_고양이.java
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
