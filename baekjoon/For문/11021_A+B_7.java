0289085cd9713db5d2aa12b22fa9b7a200064d96
smy999 <33407191+smy999@users.noreply.github.com>
2021-01-19 20:57:06 +0900

    Create 11021_A+B_7.java

---
+++ baekjoon/For문/11021_A+B_7.java
@@ -0,0 +1,19 @@
+// 11021_A+B_7.java
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        
+        int num1, num2;
+        
+        for(int i = 1; i <= cnt; i++){
+            num1 = sc.nextInt();
+            num2 = sc.nextInt();
+            
+            System.out.println("Case #" + i + ": " + (num1 + num2));
+        }
+    }
+}
