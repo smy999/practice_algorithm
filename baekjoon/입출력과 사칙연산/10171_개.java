e2faf2c1e7688c484f204d1759cc3d8a3fe30014
smy999 <33407191+smy999@users.noreply.github.com>
2021-01-19 15:42:58 +0900

    Create 10171_개.java

---
+++ baekjoon/입출력과 사칙연산/10171_개.java
@@ -0,0 +1,7 @@
+// 10171_개.java
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("|\\_/|\n" + "|q p|   /}\n" + "( 0 )\"\"\"\\\n" + "|\"^\"`    |\n" + "||_/=\\\\__|");
+    }
+}
