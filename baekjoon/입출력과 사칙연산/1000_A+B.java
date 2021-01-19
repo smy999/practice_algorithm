dcefbc6196747c63f6c70932b6ba0c2f1bbff3cb
smy999 <33407191+smy999@users.noreply.github.com>
2021-01-19 15:56:27 +0900

    Create 1000_A+B.java

---
+++ baekjoon/입출력과 사칙연산/1000_A+B.java
@@ -0,0 +1,15 @@
+// 1000_A+B.java
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        int num1, num2;
+        
+        Scanner sc = new Scanner(System.in);
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        
+        System.out.println(num1+num2);
+    }
+}
