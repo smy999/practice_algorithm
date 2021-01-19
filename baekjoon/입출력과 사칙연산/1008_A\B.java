e8a3d64b55ce5d5ebc297280a18e22310155df65
smy999 <33407191+smy999@users.noreply.github.com>
2021-01-19 16:20:30 +0900

    Create 1008_A\B.java

---
+++ baekjoon/입출력과 사칙연산/1008_A\B.java
@@ -0,0 +1,14 @@
+// 1008_A\B.java
+
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        
+        System.out.println(num1/num2);
+    }
+}
