9c8ac08ef58f02fb15276fc0abb6bf705c700a7e
smy999 <33407191+smy999@users.noreply.github.com>
2021-01-19 18:01:31 +0900

    Create 2884_알람시계.java

---
+++ baekjoon/If문/2884_알람시계.java
@@ -0,0 +1,22 @@
+// 2884_알람시계.java
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+        
+        M -= 45;
+        if(M < 0){
+            M += 60;
+            H--;
+        }
+        if(H < 0){
+            H += 24;
+        }
+        
+        System.out.println(H + " " + M);
+    }
+}
