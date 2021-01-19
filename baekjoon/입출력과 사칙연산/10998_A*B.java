eccbdd75f356937a2bc198644720f374776640c0
smy999 <33407191+smy999@users.noreply.github.com>
2021-01-19 16:02:59 +0900

    Create 10998_A*B.java

---
+++ baekjoon/입출력과 사칙연산/10998_A*B.java
@@ -0,0 +1,15 @@
+// 10998_A*B.java
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        int num1, num2;
+        
+        Scanner sc = new Scanner(System.in);
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        
+        System.out.println(num1*num2);
+    }
+}
