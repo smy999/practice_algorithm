dff33a65b2a2acee5083fdd09cfb367e81d72580
smy999 <33407191+smy999@users.noreply.github.com>
2021-01-20 22:59:27 +0900

    Create 2562_최대값.java

---
+++ baekjoon/1차원 배열/2562_최대값.java
@@ -0,0 +1,27 @@
+// 2562_최대값.java
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[9];
+        
+        int max = 0;
+        int index = 0;
+        
+        for(int i = 0; i < 9; i++){
+            arr[i] = sc.nextInt();
+            
+            if(arr[i] > max){
+                max = arr[i];
+                index = i;
+            }
+        }
+        
+        System.out.println(max);
+        System.out.println(index+1);
+        
+        sc.close();
+    }
+}
