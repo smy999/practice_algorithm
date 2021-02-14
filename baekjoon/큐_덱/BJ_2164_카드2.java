fd0c744ae5604751ab7d6547ece1dbb4f74271cf
smy999 <33407191+smy999@users.noreply.github.com>
2021-02-14 19:07:11 +0900

    Update BJ_2164_카드2.java

--- baekjoon/큐_덱/BJ_2164_카드2.java
+++ baekjoon/큐_덱/BJ_2164_카드2.java
@@ -5,29 +5,32 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class BJ_2164_카드2 {
-	
+
 	public static void main(String[]args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringBuilder sb = new StringBuilder();
-		
+
 		int N = Integer.parseInt(br.readLine());
-		
+
 		Queue<Integer> queue = new LinkedList<>();
-		
+
 		for(int i = 1; i <= N; i++) {
 			queue.add(i);
 		}
-		
-		while(true) {
+
+		while(queue.size() > 1) {
 			queue.poll();
-			
-			if(queue.size() == 1) break;
-			
-			queue.add(queue.poll());
+			queue.offer(queue.poll());	// add보다 offer가 미세하게 더 빨랐다.
 		}
-		
+
+		// 메모리 초과
+		//		while(true) {
+		//			queue.poll();
+		//			if(queue.size() == 1) break;
+		//			queue.add(queue.poll());
+		//		}
+
+
 		System.out.print(queue.poll());
 	}
 }
