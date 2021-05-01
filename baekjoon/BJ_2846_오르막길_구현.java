f51112ab3c267e03fb714698ae2056c202af8d8a
smy999 <33407191+smy999@users.noreply.github.com>
2021-05-02 01:11:17 +0900

    Update and rename BJ_2846_오르막길.java to BJ_2846_오르막길_구현.java

--- baekjoon/BJ_2846_오르막길_구현.java
+++ baekjoon/BJ_2846_오르막길_구현.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BJ_2846_오르막길 {
+public class BJ_2846_오르막길_구현 {
 
 	public static void main(String[] args) throws Exception {
 		
