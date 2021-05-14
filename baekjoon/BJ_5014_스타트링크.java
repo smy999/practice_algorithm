a49651160be2ce81ef25c5dacf0a4d65b8fe7fe7
smy999 <33407191+smy999@users.noreply.github.com>
2021-05-14 22:42:08 +0900

    Update and rename BJ_5014_스타트링크_틀림미완.java to BJ_5014_스타트링크.java

--- baekjoon/BJ_5014_스타트링크.java
+++ baekjoon/BJ_5014_스타트링크.java
@@ -7,15 +7,15 @@ import java.util.LinkedList;
 public class BJ_5014_스타트링크 {
 
 	//  전체 층 수, 강호 위치, 스타트링크 위치, 버튼업, 버튼다운
-	static int F, S, G, U, D, cnt = 0;
+	static int F, S, G, U, D, cnt = Integer.MAX_VALUE;
 	static boolean canGo = false;
-	static Queue<Integer> queue = new LinkedList<>();
+	static Queue<Info> queue = new LinkedList<>();
 	static boolean[][] visited;
 	static class Info{
-		int floor, button;
-		public Info(int floor, int button) {
+		int floor, move;
+		public Info(int floor, int move) {
 			this.floor = floor;
-			this.button = button;	// U: 0, D: 1
+			this.move = move;
 		}
 	}
 	
@@ -24,16 +24,17 @@ public class BJ_5014_스타트링크 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		F = Integer.parseInt(st.nextToken());
-		G = Integer.parseInt(st.nextToken());
 		S = Integer.parseInt(st.nextToken());
+		G = Integer.parseInt(st.nextToken());
 		U = Integer.parseInt(st.nextToken());
 		D = Integer.parseInt(st.nextToken());
 		
 		visited = new boolean[F+1][2];
 		
-		queue.add(G);
-		visited[G][0] = true;
-		visited[G][1] = true;
+		// 출발: S
+		queue.add(new Info(S, 0));
+		visited[S][0] = true;
+		visited[S][1] = true;
 		
 		bfs();
 		
@@ -43,26 +44,29 @@ public class BJ_5014_스타트링크 {
 	
 	private static void bfs() {
 		while(!queue.isEmpty()) {
-			int cur = queue.poll();
+			Info info = queue.poll();
+			int floor = info.floor;
+			int move = info.move;
 			
-			if(cur == S) {
+			// 도착: G
+			if(floor == G) {
 				canGo = true;
+				cnt = move;
 				break;
 			}
 			
-			cnt++;	// queue에서 하나씩 꺼낼 때마다 이동
-			
-			int nf = cur + U; // 위로 움직일 때
-			if(nf >= 1 && nf <= F && !visited[nf][0]) {
+			// 위로 움직일 때
+			int nf = floor + U; 
+			if(nf >= 1 && nf <= F && !visited[nf][0]) { 	// 범위 안에 있고, 해당 방향으로 방문한 적이 없을 때
 				visited[nf][0] = true;
-				queue.offer(nf);
-				continue;	// 2번 들어가는 것을 방지하기 위해 continue
+				queue.offer(new Info(nf, move+1));
 			}
 			
-			nf = cur - D; // 아래로 움직일 때
-			if(nf >= 1 && nf <= F && !visited[nf][1]) {
+			// 아래로 움직일 때
+			nf = floor - D; 
+			if(nf >= 1 && nf <= F && !visited[nf][1]){
 				visited[nf][1] = true;
-				queue.offer(nf);
+				queue.offer(new Info(nf, move+1));
 			}
 		}
 	}
